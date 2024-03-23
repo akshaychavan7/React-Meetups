5210db4aa04a266c5f7d33f5866594b25d32b584
= <=>
2024-03-23 02:43:21 -0400

    finish project

--- src/pages/AllMeetups.jsx
+++ src/pages/AllMeetups.jsx
@@ -1,4 +1,6 @@
+import { json } from "react-router-dom";
 import MeetupList from "../components/meetups/MeetupList";
+import { useEffect, useState } from "react";
 
 const DUMMY_DATA = [
   {
@@ -22,10 +24,42 @@ const DUMMY_DATA = [
 ];
 
 function AllMeetups(props) {
+  const [isLoading, setIsLoading] = useState(true);
+  const [loadedMeetups, setLoadedMeetups] = useState([]);
+
+  const URL = "https://react-meetups-d03c1-default-rtdb.firebaseio.com/"; // firebase database
+  const collectionName = "meetups.json";
+
+  useEffect(() => {
+    setIsLoading(true);
+    fetch(URL + collectionName)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        const meetups = [];
+
+        for (let key in data) {
+          meetups.push({ id: key, ...data[key] });
+        }
+
+        setIsLoading(false);
+        setLoadedMeetups(meetups);
+      });
+  }, []);
+
+  if (isLoading) {
+    return (
+      <section>
+        <p>Loading...</p>
+      </section>
+    );
+  }
+
   return (
     <div>
       <h1>All Meetups</h1>
-      <MeetupList meetups={DUMMY_DATA} />;
+      <MeetupList meetups={loadedMeetups} />
     </div>
   );
 }
--- src/pages/NewMeetup.jsx
+++ src/pages/NewMeetup.jsx
@@ -1,10 +1,23 @@
+import { useNavigate } from "react-router-dom";
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetup() {
+  const navigate = useNavigate();
+  function addMeetupHandler(meetupData) {
+    const URL = "https://react-meetups-d03c1-default-rtdb.firebaseio.com/"; // firebase database
+    const collectionName = "meetups.json";
+    fetch(URL + collectionName, {
+      method: "POST",
+      body: JSON.stringify(meetupData),
+      headers: { "Content-Type": "application/json" },
+    }).then(() => {
+      navigate("/", { replace: true });
+    });
+  }
   return (
     <section>
       <h1>Add New Meetup</h1>
-      <NewMeetupForm />
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
     </section>
   );
 }
