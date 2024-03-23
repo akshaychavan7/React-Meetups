2e95d63afb88f3eed22e4d7ab6d81acf84a30309
= <=>
2024-03-23 02:43:26 -0400

    finish project

--- src/components/layout/MainNavigation.jsx
+++ src/components/layout/MainNavigation.jsx
@@ -1,7 +1,10 @@
 import { Link } from "react-router-dom";
 import classes from "./MainNavigation.module.css";
+import FavoritesContext from "../../store/favorites-context";
+import { useContext } from "react";
 
 function MainNavitation(props) {
+  const favoritesCtx = useContext(FavoritesContext);
   return (
     <header className={classes.header}>
       <div className={classes.logo}>React Meetups</div>
@@ -14,7 +17,10 @@ function MainNavitation(props) {
           <Link to="/new-meetup">New Meetups</Link>
         </li>
         <li>
-          <Link to="/favorite">Favorites</Link>
+          <Link to="/favorite">
+            Favorites{" "}
+            <span className={classes.badge}>{favoritesCtx.totalFavorites}</span>
+          </Link>
         </li>
       </ul>
     </header>
